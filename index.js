c0de27681d44bc02792751cf29200f74998069b1
Robin Drexler <drexler.robin@gmail.com>
2015-09-01 18:33:05 +0200

    Add cors header for ajax requests

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/:seconds', function (req, res) {
   var seconds = parseFloat(req.params.seconds);
 
